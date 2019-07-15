7fc5a9c1be841aa4b6e997a041289cb766012990
JulioPernett <jjpernett2008@gmail.com>
2019-07-15 11:23:38 -0500

    se agregan las vistas agregar telefono y listar, se definen las combinaciones de colores y los string basicos

---
+++ app/src/main/java/com/example/inventaryapp/Agregar_telefonos.java
@@ -0,0 +1,14 @@
+package com.example.inventaryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Agregar_telefonos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_telefonos);
+    }
+}
