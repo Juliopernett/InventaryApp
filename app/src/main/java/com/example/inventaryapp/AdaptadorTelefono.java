98e7c6a6bdb97eaeb4fe4036dca36573072d124b
JulioPernett <jjpernett2008@gmail.com>
2019-07-15 16:44:46 -0500

    se codifica la clase AdaptadorTelefono

---
+++ app/src/main/java/com/example/inventaryapp/AdaptadorTelefono.java
@@ -0,0 +1,59 @@
+package com.example.inventaryapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdaptadorTelefono extends  RecyclerView.Adapter<AdaptadorTelefono.TelefonoViewHolder> {
+    private ArrayList<Telefono> telefonos;
+
+    public  AdaptadorTelefono(ArrayList<Telefono>telefonos){
+        this.telefonos=telefonos;
+    }
+
+    @Override
+    public TelefonoViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        //vas a usar el layaut que ya cree
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_telefonos, viewGroup, false);
+        return new TelefonoViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TelefonoViewHolder telefonoViewHolder, int i) {
+        Telefono p = telefonos.get(i);
+        telefonoViewHolder.foto.setImageResource(p.getFoto());
+        telefonoViewHolder.codigo.setText(p.getCodigo());
+        telefonoViewHolder.marca.setText(p.getMarca());
+        telefonoViewHolder.modelo.setText(p.getModelo());
+        telefonoViewHolder.cantidad.setText(p.getCantidad());
+    }
+
+    @Override
+    public int getItemCount() {
+        return telefonos.size();
+    }
+
+    public static class TelefonoViewHolder extends RecyclerView.ViewHolder{
+        private ImageView foto;
+        private TextView codigo, marca, modelo, cantidad;
+        private View v;
+
+        public  TelefonoViewHolder(View itemView){
+            super(itemView);
+            v = itemView;
+            foto = v.findViewById(R.id.foto);
+            codigo = v.findViewById(R.id.lblcodigo);
+            marca = v.findViewById(R.id.lblMarca);
+            modelo = v.findViewById(R.id.lblModelo);
+            cantidad = v.findViewById(R.id.lblCantidad);
+        }
+    }
+}
+
